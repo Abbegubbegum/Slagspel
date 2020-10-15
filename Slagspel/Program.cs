3228a52275e41de1ee9d3df3b04aadefcaa241e6
Albin <albin.nojback@gmail.com>
2020-10-15 14:35:42 +0200

    Adding miss chance and comments

--- Slagspel/Program.cs
+++ Slagspel/Program.cs
@@ -15,11 +15,11 @@ namespace Slagspel
             int fighterHp2 = 100;
             bool nameCorrect = false;
             string[] fighter2Names = { "KLEVIN", "BOB", "LUKE" };
-            fighterName2 = fighter2Names[r.Next(0, 3)];
             string ans = "";
 
             while (game)
             {
+                //Resetting variables
                 ans = "";
                 fighterHp1 = 100;
                 fighterHp2 = 100;
@@ -41,7 +41,7 @@ namespace Slagspel
                 {
                     System.Console.WriteLine("That's a weird name, only 3 to 10 characters man");
                     fighterName1 = Console.ReadLine().ToUpper();
-                    if (fighterName1.Length > 1 && fighterName1.Length < 11)
+                    if (fighterName1.Length > 2 && fighterName1.Length < 11)
                     {
                         nameCorrect = true;
                     }
@@ -63,7 +63,7 @@ namespace Slagspel
                     ans = Console.ReadLine().ToUpper();
                 }
 
-
+                //Cancelling the loop
                 if (ans == "N")
                 {
                     System.Console.WriteLine("See Ya!");
@@ -115,36 +115,90 @@ namespace Slagspel
             Console.ReadLine();
         }
 
+        //The entire automated fight inputing the names and their hp
         static void Fight(string n1, string n2, int hp1, int hp2)
         {
+            //Declaring variables
             Random r = new Random();
             int fighterPunch1 = 0;
             int fighterPunch2 = 0;
             int rounds = 0;
+            bool miss1 = false;
+            bool miss2 = false;
+
             //Subtract damage each round until one dies
             while (hp1 > 0 && hp2 > 0)
             {
-                fighterPunch1 = r.Next(0, 20);
-                fighterPunch2 = r.Next(0, 20);
-                hp1 -= fighterPunch2;
-                hp2 -= fighterPunch1;
-                System.Console.WriteLine(n1 + " has " + hp1 + " hp left!");
-                System.Console.WriteLine(n2 + " has " + hp2 + " hp left!");
+                //Raandomising the punch damage
+                fighterPunch1 = r.Next(1, 21);
+                fighterPunch2 = r.Next(1, 21);
+
+                //Randomising miss chance for both
+                if (r.Next(0, 8) == 0)
+                {
+                    miss1 = true;
+                }
+                if (r.Next(0, 8) == 0)
+                {
+                    miss2 = true;
+                }
+
+                //Subtracting the damage and outputting logs depending on if they missed or not, player 1
+                if (miss1 == false)
+                {
+                    hp2 -= fighterPunch1;
+                    if (hp2 <= 0)
+                    {
+                        System.Console.WriteLine("You hit " + n2 + " for " + fighterPunch1 + " damage killing him for good!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("You hit " + n2 + " for " + fighterPunch1 + " damage leaving him with " + hp2 + " hp left!");
+                    }
+
+                }
+                else
+                {
+                    System.Console.WriteLine("You missed!");
+                    miss1 = false;
+                }
+
+
+                //Subtracting the damage and outputting logs depending on if they missed or not and make sure they're not dead, player 2
+                if (miss2 == false && hp2 > 0)
+                {
+                    hp1 -= fighterPunch2;
+                    if (hp1 <= 0)
+                    {
+                        System.Console.WriteLine(n2 + " hit you for " + fighterPunch2 + " damage killing you!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine(n2 + " hit you for " + fighterPunch2 + " damage leaving you with " + hp1 + " hp left!");
+                    }
+                }
+                else if (hp2 > 0)
+                {
+                    System.Console.WriteLine(n2 + " missed!");
+                    miss2 = false;
+                }
+
+                //Incrementing the roundcount
                 rounds++;
             }
 
-            //Give Results
-            if (hp1 < 0 && hp2 < 0)
+            //Give Final Results
+            if (hp1 <= 0 && hp2 <= 0)
             {
                 System.Console.WriteLine("THEY BOTH DIED ON ROUND: " + rounds);
             }
-            else if (hp2 > 0)
+            else if (hp1 > 0)
             {
-                System.Console.WriteLine(n1 + " WON WITH " + hp2 + " HEALTH REMAINING ON ROUND " + rounds);
+                System.Console.WriteLine("YOU WON WITH " + hp1 + " HEALTH REMAINING ON ROUND " + rounds);
             }
             else
             {
-                System.Console.WriteLine(n1 + " WON WITH " + hp1 + " HEALTH REMAINING ON ROUND " + rounds);
+                System.Console.WriteLine(n2 + " WON WITH " + hp2 + " HEALTH REMAINING ON ROUND " + rounds);
             }
         }
     }
